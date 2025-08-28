004a072e7a99d0110e0c4693fc7226bded0fedc8
AkeshaTUT <rarizoro228@gmail.com>
2025-08-28 12:49:03 +0500

    first commit

---
+++ classwork1/src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome to my ProfileCard</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ classwork1/src/components/profile.js
@@ -0,0 +1,10 @@
+function ProfileCard() {
+    return (
+        <div className="profile-card">
+            <img src="avatar.png" alt="Avatar" />
+            <h2> Akezhan Seiktassym</h2>
+            <p>Software Engineering RU</p>
+        </div>
+    );
+}
+export default ProfileCard;
